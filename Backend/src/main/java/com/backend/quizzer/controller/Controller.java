84e64874cf6b2051e0751aade6bb3776fd0cd09c
Jay Varshney <varshneyjay2@gmail.com>
2026-03-15 16:49:04 +0530

    updated controller

--- Backend/src/main/java/com/backend/quizzer/controller/Controller.java
+++ Backend/src/main/java/com/backend/quizzer/controller/Controller.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("chat")
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class Controller {
 
     private final ChatService chatService;
