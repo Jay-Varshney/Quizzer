d1a49aaad288feb00f58741da645052b5dd26e27
Jay Varshney <varshneyjay2@gmail.com>
2026-03-24 10:33:11 +0530

    again CORS update

--- Backend/src/main/java/com/backend/quizzer/controller/Controller.java
+++ Backend/src/main/java/com/backend/quizzer/controller/Controller.java
@@ -7,6 +7,7 @@ import com.backend.quizzer.dto.Evaluation;
 import com.backend.quizzer.dto.Question;
 import com.backend.quizzer.service.ChatService;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
